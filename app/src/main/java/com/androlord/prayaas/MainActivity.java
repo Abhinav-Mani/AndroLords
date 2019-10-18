83c5fa5c3ec05f3ba03896ff0eab739351918552
Rishabh <rishabhbhardwaj1234567@gmail.com>
2019-10-19 01:18:12 +0530

    basic UI designed 1st phase value imported and images added

--- app/src/main/java/com/androlord/prayaas/MainActivity.java
+++ app/src/main/java/com/androlord/prayaas/MainActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.androlord.prayaas.Support.Login;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu,menu);
+        return true;
     }
 
     private void init() {
